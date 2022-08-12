1dd9b2a9f12b9ef20246838080b154537dcfb2e0
Braien Machado de Paula <braienmp@outlook.com>
2022-08-12 10:37:17 -0300

    test: role_controller unit tests

---
+++ src/tests/settings.py
@@ -0,0 +1,46 @@
+from fastapi.testclient import TestClient
+from unittest import TestCase
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.main import app
+from src.db.settings.config import Base, get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    connection = engine.connect()
+
+    connection.begin()
+
+    db = TestingSessionLocal(bind=connection)
+
+    yield db
+    
+    db.rollback()
+    connection.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+class BaseTestCase(TestCase):
+    def setUp(self) -> None:
+        self.session = next(override_get_db())
+
+
+class ApiBaseTestCase(TestCase):
+    client: TestClient
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = TestClient(app)        
---
+++ src/tests/unit_tests/controllers/test_role_controller.py
@@ -0,0 +1,37 @@
+from unittest.mock import patch
+
+from src.exceptions.excepetions import NotFoundException
+from src.db.cruds.role_crud import RoleCRUD
+from src.controllers.role_controller import RoleController
+from src.tests.settings import BaseTestCase
+from src.tests.mocks.role_mocks import roles, invalid_role_id, valid_role_id
+
+
+class RoleControllerTestClass(BaseTestCase):
+    @patch.object(RoleCRUD, 'list', return_value=roles)
+    def test_handle_list(self, mock):
+        '''
+          Test return of all roles from controller
+        '''
+        result = RoleController().handle_list(db=self.session)
+        self.assertEqual(roles, result)
+
+
+    @patch.object(RoleCRUD, 'get', return_value=roles[0])
+    def test_handle_get_when_id_found(self, mock):
+        '''
+          Test return of single role when id found
+        '''
+        result = RoleController().handle_get(db=self.session, exception_message='', object_id=valid_role_id)
+        self.assertEqual(roles[0], result)
+
+
+    @patch.object(RoleCRUD, 'get', return_value=None)
+    def test_handle_get_when_id_not_found(self, mock):
+        '''
+          Test raise exception when role id not found
+        '''
+        with self.assertRaises(NotFoundException) as error:
+            RoleController().handle_get(db=self.session, exception_message='Error', object_id=invalid_role_id)
+        exception = error.exception
+        self.assertEqual('Error', exception.detail)
