df12f0a8ecda8bb46788ecb981d6d93a47c0350f
Braien Machado de Paula <braienmp@outlook.com>
2022-08-15 19:05:57 -0300

    test: fix lint issues

--- src/tests/unit_tests/controllers/test_user_controller.py
+++ src/tests/unit_tests/controllers/test_user_controller.py
@@ -29,7 +29,10 @@ class UserControllerTestClass(BaseTestCase):
         user_info_with_invalid_role_id = {**user_create_data, 'role_id': invalid_role_id}
 
         with self.assertRaises(NotFoundException) as error:
-            UserController().handle_create(db=self.session, data=UserBase(**user_info_with_invalid_role_id))
+            UserController().handle_create(
+                db=self.session,
+                data=UserBase(**user_info_with_invalid_role_id)
+            )
 
         exception = error.exception
         self.assertEqual('Role não encontrada.', exception.detail)
@@ -42,7 +45,10 @@ class UserControllerTestClass(BaseTestCase):
         user_info_with_invalid_phone = {**user_create_data, 'phone': invalid_phone}
 
         with self.assertRaises(BadRequestException) as error:
-            UserController().handle_create(db=self.session, data=UserBase(**user_info_with_invalid_phone))
+            UserController().handle_create(
+                db=self.session,
+                data=UserBase(**user_info_with_invalid_phone)
+            )
 
         exception = error.exception
         self.assertEqual('Telefone invalido.', exception.detail)
@@ -52,33 +58,49 @@ class UserControllerTestClass(BaseTestCase):
         '''
           Should raise exception when invalid document
         '''
-        user_info_with_invalid_document = {**user_create_data, 'document': invalid_document}
+        user_info_with_invalid_document = {
+            **user_create_data,
+            'document': invalid_document
+        }
 
         with self.assertRaises(BadRequestException) as error:
-            UserController().handle_create(db=self.session, data=UserBase(**user_info_with_invalid_document))
+            UserController().handle_create(
+                db=self.session,
+                data=UserBase(**user_info_with_invalid_document)
+            )
 
         exception = error.exception
         self.assertEqual('Documento invalido.', exception.detail)
 
     @patch.object(UserCRUD, 'get_user_document_or_email', return_value=user_db_response)
     @patch.object(RoleController, 'handle_get', return_value=None)
-    def test_handle_create_when_invalid_document(self, RoleController_mock, UserCRUD_mock):
+    def test_handle_create_when_document_or_email_in_use(
+        self,
+        RoleController_mock,
+        UserCRUD_mock
+    ):
         '''
           Should raise exception when document or email already in use
         '''
         with self.assertRaises(BadRequestException) as error:
-            UserController().handle_create(db=self.session, data=UserBase(**user_create_data))
+            UserController().handle_create(
+                db=self.session,
+                data=UserBase(**user_create_data)
+            )
 
         exception = error.exception
         self.assertEqual('Documento ou Email já cadastrado.', exception.detail)
 
     @patch.object(RoleController, 'handle_get', return_value=None)
-    def test_handle_create_when_invalid_document(self, RoleController_mock):
+    def test_handle_create_when_fields_missing(self, RoleController_mock):
         '''
           Should raise exception when required fields are missing
         '''
         with self.assertRaises(BadRequestException) as error:
-            UserController().handle_create(db=self.session, data=UserBase(**invalid_user_create_data))
+            UserController().handle_create(
+                db=self.session,
+                data=UserBase(**invalid_user_create_data)
+            )
 
         exception = error.exception
         self.assertEqual('Campos obrigatorios: (name, password) .', exception.detail)
@@ -89,14 +111,24 @@ class UserControllerTestClass(BaseTestCase):
         '''
           Should return created user when valid user data
         '''
-        result = UserController().handle_create(db=self.session, data=UserBase(**user_create_data))
+        result = UserController().handle_create(
+            db=self.session,
+            data=UserBase(**user_create_data)
+        )
         self.assertEqual(user_db_response, result)
 
     @patch.object(UserCRUD, 'create', return_value=user_db_response_no_phone)
     @patch.object(RoleController, 'handle_get', return_value=None)
-    def test_handle_create_when_valid_user_data_but_no_phone(self, RoleController_mock, UserCRUD_mock):
+    def test_handle_create_when_valid_user_data_but_no_phone(
+        self,
+        RoleController_mock,
+        UserCRUD_mock
+    ):
         '''
           Should return created user when valid user data without phone
         '''
-        result = UserController().handle_create(db=self.session, data=UserBase(**user_create_data_no_phone))
+        result = UserController().handle_create(
+            db=self.session,
+            data=UserBase(**user_create_data_no_phone)
+        )
         self.assertEqual(user_db_response_no_phone, result)
