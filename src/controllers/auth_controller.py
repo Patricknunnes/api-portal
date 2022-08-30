a3e78288d19cb073db310b9914964937559d7c69
caio.fonseca <caio.fonseca@bravi.com.br>
2022-08-30 13:38:54 -0300

    refact: clean document in login

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -1,3 +1,4 @@
+import re
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
@@ -13,7 +14,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 class AuthController:
 
     def handle_login(self, db: Session, data_login: LoginBase) -> TokenResponse:
-        user = UserCRUD().get_user_document_or_email(db=db, document=data_login.document)
+        document = re.sub(r'\W+', '', data_login.document)
+        user = UserCRUD().get_user_document_or_email(db=db, document=document)
 
         if not user or not verify_password(data_login.password, user.password):
             raise BadRequestException(detail='Documento ou senha invalidos.')
