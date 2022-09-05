4ea81ae81595529a45e0b476cf02cd754cafac30
caio.fonseca <caio.fonseca@bravi.com.br>
2022-09-05 11:07:06 -0300

    feat: add data user in response at the route token

--- src/schemas/auth_schema.py
+++ src/schemas/auth_schema.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 
 from src.schemas.role_schema import RoleResponse
+from src.schemas.user_schema import UserResponse
 
 
 class TokenResponse(BaseModel):
+    user: UserResponse
     access_token: str
     token_type: str = 'Bearer'
 
