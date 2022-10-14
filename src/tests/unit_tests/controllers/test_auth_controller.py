cc5366d30657a5fbd4b63349ad08d6e13d0248cc
Braien Machado de Paula <braienmp@outlook.com>
2022-10-14 08:14:20 -0300

    test: auth_controller handle_sso_totvs

--- src/tests/unit_tests/controllers/test_auth_controller.py
+++ src/tests/unit_tests/controllers/test_auth_controller.py
@@ -4,7 +4,8 @@ from src.db.cruds.user_crud import UserCRUD
 from src.dependencies.totvs.soap_api import TotvsWebServer
 from src.db.models.user_model import UserModel
 from src.exceptions.exceptions import BadRequestException
-from src.schemas.auth_schema import LoginBase, TokenResponse
+from src.schemas.auth_schema import LoginBase, ResponseSsoTotvs, TokenResponse
+from src.schemas.user_schema import UserResponse
 from src.controllers.auth_controller import AuthController
 from src.tests.settings import BaseTestCase
 from src.tests.mocks.auth_mocks import (
@@ -110,3 +111,38 @@ class AuthControllerTestClass(BaseTestCase):
             'Documento ou senha inválidos.',
             exception.detail
         )
+
+    @patch.object(
+        UserCRUD,
+        'get',
+        return_value=UserModel(**user_db_response)
+    )
+    def test_handle_sso_totvs_with_non_totvs_user(self, get_mock):
+        '''
+        Should return ResponseSsoTotvs instance with all fields values as None
+        '''
+        result = AuthController().handle_sso_totvs(
+            db=self.session,
+            profile=UserResponse(**user_db_response)
+        )
+        self.assertTrue(isinstance(result, ResponseSsoTotvs))
+        self.assertEqual(result.user_name, None)
+        self.assertEqual(result.key_totvs, None)
+
+    @patch('src.controllers.auth_controller.decode_password', return_value='key_totvs')
+    @patch.object(
+        UserCRUD,
+        'get',
+        return_value=UserModel(**totvs_user_db_response)
+    )
+    def test_handle_sso_totvs_with_totvs_user(self, get_mock, decode_mock):
+        '''
+        Should return ResponseSsoTotvs instance with username and key_totvs
+        '''
+        result = AuthController().handle_sso_totvs(
+            db=self.session,
+            profile=UserResponse(**totvs_user_db_response)
+        )
+        self.assertTrue(isinstance(result, ResponseSsoTotvs))
+        self.assertEqual(result.user_name, totvs_user_db_response['username'])
+        self.assertEqual(result.key_totvs, 'key_totvs')
