ca5d7cb883ac116e8a9aae814a3605a6506e2d0c
Braien Machado de Paula <braienmp@outlook.com>
2022-12-14 17:26:05 -0300

    test: ClientCRUD - create - get

---
+++ src/tests/mocks/client_mocks.py
@@ -0,0 +1,13 @@
+client_with_secret = {
+    'id': '34930736-8424-4254-84f5-92dd68e771ef',
+    'client_id': 'client1',
+    'client_secret': 'client1_secret',
+    'redirect_uri': 'http://redirect_uri/client1.com'
+}
+
+client_without_secret = {
+    'id': '34930736-8424-4254-84f5-92dd68e771ef',
+    'client_id': 'client2',
+    'client_secret': None,
+    'redirect_uri': 'http://redirect_uri/client2.com'
+}
