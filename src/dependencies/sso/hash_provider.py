e58f3197ed8033f9bc8f4422240dbcac1ceba24b
Braien Machado de Paula <braienmp@outlook.com>
2022-12-15 09:06:21 -0300

    feat: verify secret when needed

---
+++ src/dependencies/sso/hash_provider.py
@@ -0,0 +1,22 @@
+import os
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+
+from src.settings.settings import BASE_DIR
+
+load_dotenv(os.path.join(BASE_DIR, '../../.env'))
+
+fernet = Fernet(os.getenv("SECRET_KEY_PASSWD"))
+
+
+def get_password_hash(password):
+    return fernet.encrypt(password.encode()).decode()
+
+
+def verify_password(plain_password, hashed_password):
+    decode = decode_password(hashed_password)
+    return decode == plain_password
+
+
+def decode_password(hashed_password):
+    return fernet.decrypt(hashed_password.encode()).decode()
--- src/dependencies/sso/validators.py
+++ src/dependencies/sso/validators.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 
 from src.dependencies.sso.sso_utils import AuthRequestParameters
 from src.db.cruds.client_crud import ClientCRUD
+from src.dependencies.sso.hash_provider import verify_password
 
 
 class ParamsValidator:
@@ -13,9 +14,10 @@ class ParamsValidator:
 
     def check_client_secret(self, db: Session, client_id: str, client_secret: str = None):
         client = ClientCRUD().get(db=db, client_id=client_id)
-        if client.client_secret == None:
+
+        if client.client_secret is None:
             return True
-        pass
+        return verify_password(client_secret, client.client_secret)
 
     def check_scope(self, request_scope: str):
         return 'openid' in request_scope
@@ -23,7 +25,7 @@ class ParamsValidator:
     def check_response_type(self, type: str):
         return 'code' == type
 
-    def check_redirection_uri(self, db: Session, client_id: str, uri: str):
+    def check_redirect_uri(self, db: Session, client_id: str, uri: str):
         client = ClientCRUD().get(db=db, client_id=client_id)
 
         return client.redirect_uri == uri
@@ -32,7 +34,7 @@ class ParamsValidator:
         return (
             self.check_client_id(db, params.client_id) and
             self.check_client_secret(db, params.client_id, params.client_secret) and
-            self.check_redirection_uri(db, params.client_id, params.redirect_uri) and
+            self.check_redirect_uri(db, params.client_id, params.redirect_uri) and
             self.check_response_type(params.response_type) and
             self.check_scope(params.scope)
         )
