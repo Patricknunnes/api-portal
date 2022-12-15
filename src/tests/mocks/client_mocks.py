ca0982c121bf1358969d490d1c33a93ccfa2d92c
Braien Machado de Paula <braienmp@outlook.com>
2022-12-15 09:07:35 -0300

    test: finish ParamsValidator tests - check_client_secret - check_redirect_uri - validate_authorize_params

--- src/tests/mocks/client_mocks.py
+++ src/tests/mocks/client_mocks.py
@@ -1,7 +1,12 @@
 client_with_secret = {
     'id': '34930736-8424-4254-84f5-92dd68e771ef',
     'client_id': 'client1',
-    'client_secret': 'client1_secret',
+    'client_secret': (
+        'gAAAAABjmwIQgchFg_BswmIDoSZW4YSf6_d'
+        'JRHpgPVEI7ZPeHIjD9IovpoU6WmydfIxg2t'
+        'WANIqoUK8kM-an67HQwntmCH9LPw=='
+    ),
+    # plain_client_secret: client1_secret
     'redirect_uri': 'http://redirect_uri/client1.com'
 }
 
@@ -10,4 +15,13 @@ client_without_secret = {
     'client_id': 'client2',
     'client_secret': None,
     'redirect_uri': 'http://redirect_uri/client2.com'
+}
+
+valid_auth_request_params = {
+    'client_id': 'client1',
+    'redirect_uri': 'http://redirect_uri/client1.com',
+    'response_type': 'code',
+    'scope': 'openid',
+    'state': 'any_jwt',
+    'client_secret': 'client1_secret'
 }
--- src/tests/unit_tests/dependencies/test_sso_params_validator.py
+++ src/tests/unit_tests/dependencies/test_sso_params_validator.py
@@ -1,12 +1,27 @@
 from unittest.mock import patch
 
 from src.db.cruds.client_crud import ClientCRUD
+from src.db.models.models import ClientModel
+from src.dependencies.sso.sso_utils import AuthRequestParameters
 from src.dependencies.sso.validators import ParamsValidator
 from src.tests.settings import BaseTestCase
-from src.tests.mocks.client_mocks import client_with_secret
+
+from src.tests.mocks.client_mocks import (
+    client_with_secret,
+    client_without_secret,
+    valid_auth_request_params
+)
 
 
 class ParamsValidatorTestClass(BaseTestCase):
+    def setUp(self) -> None:
+        self.auth_params = AuthRequestParameters(**valid_auth_request_params)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.auth_params = AuthRequestParameters(**valid_auth_request_params)
+        return super().tearDown()
+
     @patch.object(ClientCRUD, 'get', return_value=None)
     def test_check_invalid_client_id(self, _):
         result = ParamsValidator().check_client_id(
@@ -38,3 +53,119 @@ class ParamsValidatorTestClass(BaseTestCase):
     def test_check_valid_response_type(self):
         result = ParamsValidator().check_response_type(type='code')
         self.assertTrue(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_with_secret))
+    def test_check_invalid_client_secret_receiving_string(self, _):
+        result = ParamsValidator().check_client_secret(
+            db=self.session,
+            client_id=client_with_secret['client_id'],
+            client_secret='invalid_secret'
+        )
+        self.assertFalse(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_with_secret))
+    def test_check_invalid_client_secret_receiving_none(self, _):
+        result = ParamsValidator().check_client_secret(
+            db=self.session,
+            client_id=client_with_secret['client_id'],
+            client_secret=None
+        )
+        self.assertFalse(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_with_secret))
+    def test_check_valid_client_secret(self, _):
+        result = ParamsValidator().check_client_secret(
+            db=self.session,
+            client_id=client_with_secret['client_id'],
+            client_secret='client1_secret'
+        )
+        self.assertTrue(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_without_secret))
+    def test_check_client_secret_unneeded_receiving_none(self, _):
+        result = ParamsValidator().check_client_secret(
+            db=self.session,
+            client_id=client_without_secret['client_id'],
+            client_secret=None
+        )
+        self.assertTrue(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_without_secret))
+    def test_check_client_secret_unneeded_receiving_str(self, _):
+        result = ParamsValidator().check_client_secret(
+            db=self.session,
+            client_id=client_without_secret['client_id'],
+            client_secret='any_string'
+        )
+        self.assertTrue(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_without_secret))
+    def test_check_invalid_redirect_uri(self, _):
+        result = ParamsValidator().check_redirect_uri(
+            db=self.session,
+            client_id=client_without_secret['client_id'],
+            uri='invalid_uri'
+        )
+        self.assertFalse(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_without_secret))
+    def test_check_valid_redirect_uri(self, _):
+        result = ParamsValidator().check_redirect_uri(
+            db=self.session,
+            client_id=client_without_secret['client_id'],
+            uri=client_without_secret['redirect_uri']
+        )
+        self.assertTrue(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_with_secret))
+    def test_validate_valid_authorize_params(self, _):
+        result = ParamsValidator().validate_authorize_params(
+            db=self.session,
+            params=self.auth_params
+        )
+        self.assertTrue(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=None)
+    def test_validate_authorize_params_invalid_client_id(self, _):
+        self.auth_params.client_id = 'invalid'
+        result = ParamsValidator().validate_authorize_params(
+            db=self.session,
+            params=self.auth_params
+        )
+        self.assertFalse(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_with_secret))
+    def test_validate_authorize_params_invalid_client_secret(self, _):
+        self.auth_params.client_secret = 'invalid'
+        result = ParamsValidator().validate_authorize_params(
+            db=self.session,
+            params=self.auth_params
+        )
+        self.assertFalse(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_with_secret))
+    def test_validate_authorize_params_invalid_redirect_uri(self, _):
+        self.auth_params.redirect_uri = 'invalid'
+        result = ParamsValidator().validate_authorize_params(
+            db=self.session,
+            params=self.auth_params
+        )
+        self.assertFalse(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_with_secret))
+    def test_validate_authorize_params_invalid_response_type(self, _):
+        self.auth_params.response_type = 'invalid'
+        result = ParamsValidator().validate_authorize_params(
+            db=self.session,
+            params=self.auth_params
+        )
+        self.assertFalse(result)
+
+    @patch.object(ClientCRUD, 'get', return_value=ClientModel(**client_with_secret))
+    def test_validate_authorize_params_invalid_scope(self, _):
+        self.auth_params.scope = 'invalid'
+        result = ParamsValidator().validate_authorize_params(
+            db=self.session,
+            params=self.auth_params
+        )
+        self.assertFalse(result)
