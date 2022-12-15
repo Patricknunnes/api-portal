1e5b079258dbed372a4eed1aeabab364ee404c84
Braien Machado de Paula <braienmp@outlook.com>
2022-12-15 12:09:46 -0300

    refactor: make validators easier to read

--- src/dependencies/sso/validators.py
+++ src/dependencies/sso/validators.py
@@ -8,14 +8,12 @@ from src.dependencies.sso.hash_provider import verify_password
 class ParamsValidator:
     def check_client_id(self, db: Session, client_id: str):
         client = ClientCRUD().get(db=db, client_id=client_id)
-        if client:
-            return True
-        return False
+        return True if client else False
 
     def check_client_secret(self, db: Session, client_id: str, client_secret: str = None):
         client = ClientCRUD().get(db=db, client_id=client_id)
 
-        if client.client_secret is None:
+        if client and client.client_secret is None:
             return True
         return verify_password(client_secret, client.client_secret)
 
@@ -28,23 +26,23 @@ class ParamsValidator:
     def check_redirect_uri(self, db: Session, client_id: str, uri: str):
         client = ClientCRUD().get(db=db, client_id=client_id)
 
-        return client.redirect_uri == uri
+        return True if client and client.redirect_uri == uri else False
 
     def check_grant_type(self, type: str):
         return 'authorization_code' == type
 
     def validate_authorize_params(self, params: AuthRequestParameters, db: Session):
-        return (
-            self.check_client_id(db, params.client_id) and
-            self.check_redirect_uri(db, params.client_id, params.redirect_uri) and
-            self.check_response_type(params.response_type) and
+        return all([
+            self.check_client_id(db, params.client_id),
+            self.check_redirect_uri(db, params.client_id, params.redirect_uri),
+            self.check_response_type(params.response_type),
             self.check_scope(params.scope)
-        )
+        ])
 
     def validate_token_params(self, params: TokenRequestBody, db: Session):
-        return (
-            self.check_client_id(db, params.client_id) and
-            self.check_redirect_uri(db, params.client_id, params.redirect_uri) and
-            self.check_client_secret(db, params.client_id, params.client_secret) and
+        return all([
+            self.check_client_id(db, params.client_id),
+            self.check_redirect_uri(db, params.client_id, params.redirect_uri),
+            self.check_client_secret(db, params.client_id, params.client_secret),
             self.check_grant_type(params.grant_type)
-        )
+        ])
