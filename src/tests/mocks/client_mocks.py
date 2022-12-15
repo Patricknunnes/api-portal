bec3007dff16e457f1fa352e8e53342dda7dfc74
Braien Machado de Paula <braienmp@outlook.com>
2022-12-15 16:04:55 -0300

    test: fix client_secret in client_mocks.py

--- src/tests/mocks/client_mocks.py
+++ src/tests/mocks/client_mocks.py
@@ -2,9 +2,8 @@ client_with_secret = {
     'id': '34930736-8424-4254-84f5-92dd68e771ef',
     'client_id': 'client1',
     'client_secret': (
-        'gAAAAABjmwIQgchFg_BswmIDoSZW4YSf6_d'
-        'JRHpgPVEI7ZPeHIjD9IovpoU6WmydfIxg2t'
-        'WANIqoUK8kM-an67HQwntmCH9LPw=='
+        'gAAAAABjm297gUUedBE6Zx4nYNG7CiDilafKewZdqn5D7bF7cbp0I0uso2'
+        'YYf9ztPIPPoPqm9fFAtv_DEm0c2XvfY-cwaLh1HA=='
     ),
     # plain_client_secret: client1_secret
     'redirect_uri': 'http://redirect_uri/client1.com'
