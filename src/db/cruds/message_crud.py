a93ad724b823e7fb1e08d423929169469575f5fe
Braien Machado de Paula <braienmp@outlook.com>
2023-01-02 09:26:47 -0300

    refactor: message crud inherit pagination class

--- src/db/cruds/message_crud.py
+++ src/db/cruds/message_crud.py
@@ -1,7 +1,7 @@
-from src.db.cruds.base import BaseCRUD
+from src.db.cruds.pagination_oriented_crud import PaginationOrientedCRUD
 from src.db.models.models import MessageModel
 
 
-class MessageCRUD(BaseCRUD):
+class MessageCRUD(PaginationOrientedCRUD):
     def __init__(self):
         super(MessageCRUD, self).__init__(MessageModel)
